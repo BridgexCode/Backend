77a3834d7650a2e1c3523a1a2e34de50d0305908
shibzz <monsterbegin@gmail.com>
2026-06-17 12:56:20 +0530

    feat: Refactor user service and controller for improved user management functionality

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUser } from "./user.controller.js";
 import {
-  protectRoute,
-  authorizeRoles,
-} from "../auth/auth.middleware.js";
+  createUserController,
+  softDeleteUserController,
+  updateUserController,
+} from "./user.controller.js";
+import { protectRoute, authorizeRoles } from "../auth/auth.middleware.js";
 import { Roles } from "../../common/constants/roles.js";
 
 const router = Router();
@@ -12,13 +13,10 @@ router.post(
   "/",
   protectRoute,
   authorizeRoles(Roles.ORGANIZATION_OWNER),
-  createUser
+  createUserController,
 );
-import { softDeleteUserController, updateUserController } from "./user.controller.js";
 
-const router = Router()
-
-router.patch('/:id/soft-delete',softDeleteUserController)
-router.patch('/:id/update-user',updateUserController)
+router.patch("/:id/soft-delete", softDeleteUserController);
+router.patch("/:id/update-user", updateUserController);
 
 export default router;
