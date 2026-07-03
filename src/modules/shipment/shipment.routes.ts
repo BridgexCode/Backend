98e7f8ddebd2c4c2b93ebe0d018f2da0fad5738e
unknown <sheminmuhammed594@gmail.com>
2026-07-03 10:00:18 +0530

    Implement shipment updates

--- src/modules/shipment/shipment.routes.ts
+++ src/modules/shipment/shipment.routes.ts
@@ -13,20 +13,21 @@ import { Roles } from "../../common/constants/roles.js";
 const router = Router();
 
 router.post(
-  "/",
+  "/create-shipment",
   protectRoute,
   authorizeRoles(Roles.ORGANIZATION_OWNER, Roles.OPERATIONS_MANAGER),
   createShipmentController,
 );
 
 router.get(
-  "/",
+  "/get-shipments",
   protectRoute,
   authorizeRoles(Roles.ORGANIZATION_OWNER, Roles.OPERATIONS_MANAGER),
   getShipmentsController,
 );
 
 router.get(
+<<<<<<< HEAD
   "/timeline",
   protectRoute,
   authorizeRoles(Roles.ORGANIZATION_OWNER, Roles.OPERATIONS_MANAGER),
@@ -35,6 +36,9 @@ router.get(
 
 router.get(
   "/:id",
+=======
+  "/shipmentById:id",
+>>>>>>> a19e50ee0ef048c7bc0c3ca61bbf96400984b003
   protectRoute,
   authorizeRoles(Roles.ORGANIZATION_OWNER, Roles.OPERATIONS_MANAGER),
   getShipmentByIdController,
